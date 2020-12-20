2401ac23885796943daf4ff494c524e3dbff4a3d
S. Sanneman <seandafydd@gmail.com>
2020-12-20 14:09:58 -0800

    trying out carousel

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
+import ReactCardCarousel from 'react-card-carousel';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns, Row } from 'react-bootstrap';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import './Projects.css';
+
+class MyCarousel extends Component {
+
+    static get CARD_STYLE() {
+      return {
+        height: '200px',
+        width: '200px',
+        paddingTop: '80px',
+        textAlign: 'center',
+        background: '#52C0F5',
+        color: '#FFF',
+        fontSize: '12px',
+        textTransform: 'uppercase',
+        borderRadius: '10px',
+      };
+    }
+}
+
 
 
 
@@ -11,6 +29,24 @@ const Projects = () => {
 
 <Container fluid className="d-flex">
             <Row className="main-panel">
+
+            <ReactCardCarousel autoplay={ true } autoplay_speed={ 10000 }>
+        <div style={ MyCarousel.CARD_STYLE }>
+          First Card
+        </div>
+        <div style={ MyCarousel.CARD_STYLE }>
+          Second Card
+        </div>
+        <div style={ MyCarousel.CARD_STYLE }>
+          Third Card
+        </div>
+        <div style={ MyCarousel.CARD_STYLE }>
+          Fourth Card
+        </div>
+        <div style={ MyCarousel.CARD_STYLE }>
+          Fifth Card
+        </div>
+      </ReactCardCarousel>
 
 
 
