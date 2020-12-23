f88528246cc4e4b4572f288fb0c3079184bac6cc
S. Sanneman <seandafydd@gmail.com>
2020-12-23 15:39:13 -0800

    trying to get carousel working

--- src/App.js
+++ src/App.js
@@ -31,10 +31,7 @@ function App() {
                   <Switch>
                     <Route exact path='/' component={About} />
                     <Route exact path='/projects' component={Projects} />
-                    {/* <Route exact path='/diary' component={Diary} />
                     
-                    <Route exact path='/messages' component={Messages} />
-                    <Route exact path='/contact' component={Contact} /> */}
                   </Switch>
                 </Col>
               </Container>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,58 +1,30 @@
-import React, { useState, useEffect, Component } from 'react';
-import ReactCardCarousel from 'react-card-carousel';
+import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns, Row } from 'react-bootstrap';
 import './Projects.css';
-
-class MyCarousel extends Component {
-
-    static get CARD_STYLE() {
-      return {
-        height: '200px',
-        width: '200px',
-        paddingTop: '80px',
-        textAlign: 'center',
-        background: '#52C0F5',
-        color: '#FFF',
-        fontSize: '12px',
-        textTransform: 'uppercase',
-        borderRadius: '10px',
-      };
-    }
-}
 
 
 
 
 const Projects = () => {
     return (
-        <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-center">
+        
+      <Container fluid className="d-flex">
 
-<Container fluid className="d-flex">
-            <Row className="main-panel">
-
-            <ReactCardCarousel autoplay={ true } autoplay_speed={ 10000 }>
-        <div style={ MyCarousel.CARD_STYLE }>
-          First Card
-        </div>
-        <div style={ MyCarousel.CARD_STYLE }>
-          Second Card
-        </div>
-        <div style={ MyCarousel.CARD_STYLE }>
-          Third Card
-        </div>
-        <div style={ MyCarousel.CARD_STYLE }>
-          Fourth Card
-        </div>
-        <div style={ MyCarousel.CARD_STYLE }>
-          Fifth Card
-        </div>
-      </ReactCardCarousel>
+<Row className="main-panel">
 
 
 
-            </Row>
-        </Container>
-        </div>
+
+        
+        
+      </Row>
+      </Container>
+      </div>
+        
+        
+        
+        
     )
 }
 
