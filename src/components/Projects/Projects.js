1a53af6f2c553fa9a64c0b0237970d2de1191d61
S. Sanneman <seandafydd@gmail.com>
2020-12-31 14:13:20 -0800

    working on card styling for project cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns, Row } from 'react-bootstrap';
 import './Projects.css';
+import bookGarden from '../../assets/bookgarden_scrnsht.png';
 
 
 
@@ -12,6 +13,17 @@ const Projects = () => {
       <Container fluid className="d-flex">
 
 <Row className="main-panel">
+<Card className="cards" style={{ width: '18rem' }}>
+  <Card.Img className="bookShot" variant="top" src={bookGarden} />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
 
 
 
