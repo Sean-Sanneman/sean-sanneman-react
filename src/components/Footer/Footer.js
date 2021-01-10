d26302d6ab6610d6fe420bbf7102717f841e63e9
S. Sanneman <seandafydd@gmail.com>
2021-01-09 17:40:01 -0800

    updated footer info

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,17 +20,20 @@ return (
             <div className="row">
                 <div className="col-lg-8 col-md-8 col-sm-12">
                     <h6 className="text-uppercase font-weight-bold">Additional Information</h6>
-                    <p>Catty ipsum you have cat to be kitten me right meow, but sleep on dog bed, force dog to sleep on floor, 
-                        catch mouse and gave it as a present, my slave human didn't give me any food so i pooped on the floor. </p>
+                    <p>
+                    <a className="link" href="https://www.linkedin.com/in/sean-sanneman-b99274158">LinkedIn</a>
+                    <br></br>
+                    <a className="link" href="https://github.com/Sean-Sanneman">GitHub</a>
+                    </p>
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-12">
                     <h6 className="text-uppercase font-weight-bold">Contact</h6>
-                    <p>1640 Riverside Drive, Hill Valley, California
-                        <br />info@mywebsite.com
-                        <br />+ 01 234 567 88</p>
+                    <p>Burbank, California
+                        <br></br>
+                    <a className="link" href="mailto:seandafydd@gmail.com">seandafydd@gmail.com</a></p>
                 </div>
             </div>
-            <div className="footer-copyright text-center">© 2020 Copyright: future-RAM.com</div>
+            <div className="footer-copyright text-center">© 2020 - Sean Sanneman</div>
         </div>
     </div>
 
