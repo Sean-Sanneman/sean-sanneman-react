6a6cb4a1d45502d45ca6cd27574d3e0f8cde4eff
S. Sanneman <seandafydd@gmail.com>
2021-01-11 23:01:59 -0800

    starting resume component

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns, Row } from 'react-bootstrap';
+import './Resume.css';
+
+const Resume = () => {
+    return (
+
+    )
+}
+
+export default Resume;
