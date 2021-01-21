eee43ffd8691a628a6badad87faae06c7020fd6a
S. Sanneman <seandafydd@gmail.com>
2021-01-21 13:42:10 -0800

    about page responsive,  skills and tech section added to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns, Row } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import './About.css';
 import myPhoto from "../../assets/Profile_cropped.jpg";
 
@@ -14,14 +14,11 @@ const About = () => {
                        
             <h2 className="header aboutMeText"></h2>
 
-                <br></br>
-                <br></br>
-                <br></br>
                 <div class="flex-container">
-                <div class="flex-child-left">
+                <div class="" id='myPhoto'>
                     <img src={myPhoto} className="photo" style={{ width: "75%"}} alt="myPhoto" />
                     </div>
-                    <div class="flex-row hero flex-child-right" style={{ width: "100%"}}>
+                    <div class="" id='aboutText' style={{ width: "100%"}}>
                         <h5 className="aboutMeText">
                 I am a full-Stack Web Developer with a background in audio engineering, and a passion for innovation and forward thinking design. I effectively combine creativity and problem solving to develop applications that bring something a little extra to the mix. I am known for being adept at seeing the big picture while maintaining a detail-oriented approach to anything I am working on. Should you find yourself needing someone to handle audio and video content on top of any coding required, I am able to do that as well. 
                 </h5>
@@ -29,11 +26,14 @@ const About = () => {
                 </div>
 
                 <h1><em></em></h1>
-                <br></br>
-                <br></br>
                 
-                
-                <p><h3></h3></p>
+                <Row className="main-panel">
+                <p><h3>Skills and Tech</h3></p>
+                <h5>Languages: HTML, CSS, JavaScript, JQuery, Bootstrap, Handlebars, ReactJS, MERN (MongoDB, Express, React, NodeJS), GraphQL, MySQL, NoSQL, Jest, Git
+                    <br></br>
+                    <br></br>
+                Applications: VSCode, GitBash, Insomnia, Postman, Bugzilla, MS Office, Open Office, Cubase, ProTools, Sound Forge, DAViD, Vegas. </h5>
+                </Row>
                 
             </Row>
         </Container>
